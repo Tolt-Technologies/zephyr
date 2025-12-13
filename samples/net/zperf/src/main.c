db706a5a1ec2cea70d9fdd23ee3336a5a28ed1d0
Jay Beavers <jay@tolttechnologies.com>
2025-12-12 20:15:56 -0800

    Auto-start UDP 5001 server

--- samples/net/zperf/src/main.c
+++ samples/net/zperf/src/main.c
@@ -10,6 +10,7 @@
  */
 #include <zephyr/usb/usbd.h>
 #include <zephyr/net/net_config.h>
+#include <zephyr/net/zperf.h>
 
 LOG_MODULE_REGISTER(zperf, CONFIG_NET_ZPERF_LOG_LEVEL);
 
@@ -20,6 +21,18 @@ LOG_MODULE_REGISTER(zperf, CONFIG_NET_ZPERF_LOG_LEVEL);
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
 #include <sample_usbd.h>
 #endif
+
+#define ZPERF_UDP_PORT 5001
+
+static void udp_session_cb(enum zperf_status status,
+			   struct zperf_results *result,
+			   void *user_data)
+{
+	if (status == ZPERF_SESSION_FINISHED) {
+		LOG_INF("UDP session finished: %llu bytes in %llu us",
+			result->total_len, result->time_in_us);
+	}
+}
 
 int main(void)
 {
@@ -47,5 +60,18 @@ int main(void)
 #if defined(CONFIG_NET_DHCPV4) && !defined(CONFIG_NET_CONFIG_SETTINGS)
 	net_dhcpv4_start(net_if_get_default());
 #endif
+
+	/* Auto-start UDP server for stress testing */
+	struct zperf_download_params param = {
+		.port = ZPERF_UDP_PORT,
+	};
+
+	int ret = zperf_udp_download(&param, udp_session_cb, NULL);
+	if (ret < 0) {
+		LOG_ERR("Failed to start UDP server: %d", ret);
+	} else {
+		LOG_INF("UDP server auto-started on port %d", ZPERF_UDP_PORT);
+	}
+
 	return 0;
 }
