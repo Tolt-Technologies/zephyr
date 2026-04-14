Tolt-Technologies/zephyr

commit 88f5672e4c78ffa90e3369af0823ec245a73f6a9

net/ipv6: Switch MLD join, leave and report to new net_pkt API
Most of the code had to be reworked due to the new API: it's more
logical to do everything sequentially (first headers, then MLD part)
than the contrary with inserting headers at the end.

Using get_data/set_data as well it makes the code clearer.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

subsys/net/ip/ipv6_mld.c
@@ -30,160 +30,155 @@ LOG_MODULE_DECLARE(net_ipv6, CONFIG_NET_IPV6_LOG_LEVEL);
 #include "net_stats.h"
 
 /* Timeout for various buffer allocations in this file. */
-#define NET_BUF_TIMEOUT K_MSEC(50)
+#define PKT_WAIT_TIME K_MSEC(50)
 
-#define append(pkt, type, value)					\
-	do {								\
-		if (!net_pkt_append_##type##_timeout(pkt, value,	\
-						     NET_BUF_TIMEOUT)) { \
-			ret = -ENOMEM;					\
-			goto drop;					\
-		}							\
-	} while (0)
+#define MLDv2_MCAST_RECORD_LEN sizeof(struct net_icmpv6_mld_mcast_record)
+#define IPV6_OPT_HDR_ROUTER_ALERT_LEN 8
 
-#define append_all(pkt, size, value)					\
-	do {								\
-		if (!net_pkt_append_all(pkt, size, value,		\
-					NET_BUF_TIMEOUT)) {		\
-			ret = -ENOMEM;					\
-			goto drop;					\
-		}							\
-	} while (0)
+#define MLDv2_LEN (MLDv2_MCAST_RECORD_LEN + sizeof(struct in6_addr))
 
-#define MLDv2_LEN (2 + 1 + 1 + 2 + sizeof(struct in6_addr) * 2)
-
-static struct net_pkt *create_mldv2(struct net_pkt *pkt,
-				    const struct in6_addr *addr,
-				    u16_t record_type,
-				    u8_t num_sources)
+static int mld_create(struct net_pkt *pkt,
+		      const struct in6_addr *addr,
+		      u8_t record_type,
+		      u16_t num_sources)
 {
-	int ret;
+	NET_PKT_DATA_ACCESS_DEFINE(mld_access,
+				   struct net_icmpv6_mld_mcast_record);
+	struct net_icmpv6_mld_mcast_record *mld;
 
-	append(pkt, u8, record_type);
-	append(pkt, u8, 0);             /* aux data len */
-	append(pkt, be16, num_sources); /* number of addresses */
+	mld = (struct net_icmpv6_mld_mcast_record *)
+				net_pkt_get_data_new(pkt, &mld_access);
+	if (!mld) {
+		return -ENOBUFS;
+	}
 
-	append_all(pkt, sizeof(struct in6_addr), addr->s6_addr);
+	mld->record_type = record_type;
+	mld->aux_data_len = 0;
+	mld->num_sources = htons(num_sources);
+
+	net_ipaddr_copy(&mld->mcast_address, addr);
+
+	if (net_pkt_set_data(pkt, &mld_access)) {
+		return -ENOBUFS;
+	}
 
 	if (num_sources > 0) {
 		/* All source addresses, RFC 3810 ch 3 */
-		append_all(pkt, sizeof(struct in6_addr),
-			   net_ipv6_unspecified_address()->s6_addr);
+		if (net_pkt_write_new(pkt,
+				      net_ipv6_unspecified_address()->s6_addr,
+				      sizeof(struct in6_addr))) {
+			return -ENOBUFS;
+		}
 	}
 
-	return pkt;
-
-drop:
-	return NULL;
+	return 0;
 }
 
-static int send_mldv2_raw(struct net_if *iface, struct net_buf *frags)
+static int mld_create_packet(struct net_pkt *pkt, u16_t count)
 {
-	struct net_pkt *pkt;
 	struct in6_addr dst;
-	u16_t pos;
-	int ret;
 
 	/* Sent to all MLDv2-capable routers */
 	net_ipv6_addr_create(&dst, 0xff02, 0, 0, 0, 0, 0, 0, 0x0016);
 
-	pkt = net_pkt_get_reserve_tx(NET_BUF_TIMEOUT);
-	if (!pkt) {
-		return -ENOMEM;
+	net_pkt_set_ipv6_hop_limit(pkt, 1); /* RFC 3810 ch 7.4 */
+
+	if (net_ipv6_create_new(pkt,
+				net_if_ipv6_select_src_addr(
+					net_pkt_iface(pkt), &dst),
+				 &dst)) {
+		return -ENOBUFS;
 	}
-
-	if (!net_ipv6_create(pkt,
-			     net_if_ipv6_select_src_addr(iface, &dst),
-			     &dst,
-			     iface,
-			     NET_IPV6_NEXTHDR_HBHO)) {
-		ret = -ENOMEM;
-		goto drop;
-	}
-
-	NET_IPV6_HDR(pkt)->hop_limit = 1; /* RFC 3810 ch 7.4 */
-
-	net_pkt_set_ipv6_hdr_prev(pkt, pkt->frags->len);
 
 	/* Add hop-by-hop option and router alert option, RFC 3810 ch 5. */
-	append(pkt, u8, IPPROTO_ICMPV6);
-	append(pkt, u8, 0);        /* length (0 means 8 bytes) */
-
-	/* IPv6 router alert option is described in RFC 2711. */
-	append(pkt, be16, 0x0502); /* RFC 2711 ch 2.1 */
-	append(pkt, be16, 0);      /* pkt contains MLD msg */
-	append(pkt, u8, 0);        /* padding */
-	append(pkt, u8, 0);        /* padding */
-
-	/* ICMPv6 header */
-	append(pkt, u8, NET_ICMPV6_MLDv2); /* type */
-	append(pkt, u8, 0);                /* code */
-	append(pkt, be16, 0);              /* chksum */
-	append(pkt, be16, 0);              /* reserved field */
-
-#define ROUTER_ALERT_LEN 8
-
-	net_pkt_set_iface(pkt, iface);
-
-	/* Insert the actual multicast record(s) here */
-	net_pkt_frag_add(pkt, frags);
-
-	ret = net_ipv6_finalize(pkt, NET_IPV6_NEXTHDR_HBHO);
-	if (ret < 0) {
-		goto drop;
+	if (net_pkt_write_u8_new(pkt, IPPROTO_ICMPV6) ||
+	    net_pkt_write_u8_new(pkt, 0)) {
+		return -ENOBUFS;
 	}
 
-	net_pkt_set_ipv6_ext_len(pkt, ROUTER_ALERT_LEN);
-
-	if (!net_pkt_write_be16_timeout(pkt, pkt->frags,
-					NET_IPV6H_LEN + ROUTER_ALERT_LEN + 2,
-					&pos,
-					ntohs(net_calc_chksum_icmpv6(pkt)),
-					NET_BUF_TIMEOUT)) {
-		ret = -ENOMEM;
-		goto drop;
+	/* IPv6 router alert option is described in RFC 2711.
+	 * - 0x0502 RFC 2711 ch 2.1
+	 * - MLD (value 0)
+	 * - 2 bytes of padding
+	 */
+	if (net_pkt_write_be16_new(pkt, 0x0502) ||
+	    net_pkt_write_be16_new(pkt, 0) ||
+	    net_pkt_write_be16_new(pkt, 0)) {
+		return -ENOBUFS;
 	}
 
-	ret = net_send_data(pkt);
-	if (ret < 0) {
-		goto drop;
+	net_pkt_set_ipv6_ext_len(pkt, IPV6_OPT_HDR_ROUTER_ALERT_LEN);
+
+	/* ICMPv6 header + reserved space + count.
+	 * MLDv6 stuff will come right after
+	 */
+	if (net_icmpv6_create(pkt, NET_ICMPV6_MLDv2, 0) ||
+	    net_pkt_write_be16_new(pkt, 0) ||
+	    net_pkt_write_be16_new(pkt, count)) {
+		return -ENOBUFS;
+	}
+
+	return 0;
+}
+
+static int mld_send(struct net_pkt *pkt)
+{
+	net_pkt_cursor_init(pkt);
+
+	net_ipv6_finalize_new(pkt, NET_IPV6_NEXTHDR_HBHO);
+
+	/* IPV6 finalization above could not update ICMPv6 checksum
+	 * due to the existence of Router Alert option in between.
+	 * So let's do it here:
+	 */
+	net_pkt_skip(pkt, IPV6_OPT_HDR_ROUTER_ALERT_LEN);
+
+	net_icmpv6_finalize(pkt);
+
+	if (net_send_data(pkt) < 0) {
+		net_stats_update_icmp_drop(net_pkt_iface(pkt));
+		net_stats_update_ipv6_mld_drop(net_pkt_iface(pkt));
+
+		net_pkt_unref(pkt);
+
+		return -1;
 	}
 
 	net_stats_update_icmp_sent(net_pkt_iface(pkt));
 	net_stats_update_ipv6_mld_sent(net_pkt_iface(pkt));
 
 	return 0;
-
-drop:
-	net_stats_update_icmp_drop(net_pkt_iface(pkt));
-	net_stats_update_ipv6_mld_drop(net_pkt_iface(pkt));
-
-	net_pkt_unref(pkt);
-
-	return ret;
 }
 
-static int send_mldv2(struct net_if *iface, const struct in6_addr *addr,
-		      u8_t mode)
+static int mld_send_generic(struct net_if *iface,
+			    const struct in6_addr *addr,
+			    u8_t mode)
 {
 	struct net_pkt *pkt;
 	int ret;
 
-	pkt = net_pkt_get_reserve_tx(NET_BUF_TIMEOUT);
+	pkt = net_pkt_alloc_with_buffer(iface, IPV6_OPT_HDR_ROUTER_ALERT_LEN +
+					NET_ICMPV6_UNUSED_LEN +
+					MLDv2_MCAST_RECORD_LEN +
+					sizeof(struct in6_addr),
+					AF_INET6, IPPROTO_ICMPV6,
+					PKT_WAIT_TIME);
 	if (!pkt) {
 		return -ENOMEM;
 	}
 
-	append(pkt, be16, 1); /* number of records */
-
-	if (!create_mldv2(pkt, addr, mode, 1)) {
-		ret = -ENOMEM;
+	if (mld_create_packet(pkt, 1) ||
+	    mld_create(pkt, addr, mode, 1)) {
+		ret = -ENOBUFS;
 		goto drop;
 	}
 
-	ret = send_mldv2_raw(iface, pkt->frags);
+	ret = mld_send(pkt);
+	if (ret) {
+		goto drop;
+	}
 
-	pkt->frags = NULL;
+	return 0;
 
 drop:
 	net_pkt_unref(pkt);
@@ -208,7 +203,7 @@ int net_ipv6_mld_join(struct net_if *iface, const struct in6_addr *addr)
 		}
 	}
 
-	ret = send_mldv2(iface, addr, NET_IPV6_MLDv2_MODE_IS_EXCLUDE);
+	ret = mld_send_generic(iface, addr, NET_IPV6_MLDv2_MODE_IS_EXCLUDE);
 	if (ret < 0) {
 		return ret;
 	}
@@ -230,7 +225,7 @@ int net_ipv6_mld_leave(struct net_if *iface, const struct in6_addr *addr)
 		return -EINVAL;
 	}
 
-	ret = send_mldv2(iface, addr, NET_IPV6_MLDv2_MODE_IS_INCLUDE);
+	ret = mld_send_generic(iface, addr, NET_IPV6_MLDv2_MODE_IS_INCLUDE);
 	if (ret < 0) {
 		return ret;
 	}
@@ -246,42 +241,44 @@ static void send_mld_report(struct net_if *iface)
 {
 	struct net_if_ipv6 *ipv6 = iface->config.ip.ipv6;
 	struct net_pkt *pkt;
-	int i, ret, count = 0;
+	int i, count = 0;
 
 	NET_ASSERT(ipv6);
-
-	pkt = net_pkt_get_reserve_tx(NET_BUF_TIMEOUT);
-	if (!pkt) {
-		return;
-	}
-
-	append(pkt, u8, 0); /* This will be the record count */
 
 	for (i = 0; i < NET_IF_MAX_IPV6_MADDR; i++) {
 		if (!ipv6->mcast[i].is_used || !ipv6->mcast[i].is_joined) {
 			continue;
 		}
 
-		if (!create_mldv2(pkt, &ipv6->mcast[i].address.in6_addr,
-				  NET_IPV6_MLDv2_MODE_IS_EXCLUDE, 0)) {
-			goto drop;
-		}
-
 		count++;
 	}
 
-	if (count > 0) {
-		u16_t pos;
+	pkt = net_pkt_alloc_with_buffer(iface, IPV6_OPT_HDR_ROUTER_ALERT_LEN +
+					NET_ICMPV6_UNUSED_LEN +
+					count * MLDv2_MCAST_RECORD_LEN,
+					AF_INET6, IPPROTO_ICMPV6,
+					PKT_WAIT_TIME);
+	if (!pkt) {
+		return;
+	}
 
-		/* Write back the record count */
-		if (!net_pkt_write_u8_timeout(pkt, pkt->frags, 0, &pos,
-					      count, NET_BUF_TIMEOUT)) {
-			goto drop;
+	if (mld_create_packet(pkt, count)) {
+		goto drop;
+	}
+
+	for (i = 0; i < NET_IF_MAX_IPV6_MADDR; i++) {
+		if (!ipv6->mcast[i].is_used || !ipv6->mcast[i].is_joined) {
+			continue;
 		}
 
-		send_mldv2_raw(iface, pkt->frags);
+		if (!mld_create(pkt, &ipv6->mcast[i].address.in6_addr,
+				NET_IPV6_MLDv2_MODE_IS_EXCLUDE, 0)) {
+			goto drop;
+		}
+	}
 
-		pkt->frags = NULL;
+	if (!mld_send(pkt)) {
+		return;
 	}
 
 drop:

tests/net/mld/src/main.c
@@ -96,16 +96,28 @@ static void net_test_iface_init(struct net_if *iface)
 			     NET_LINK_ETHERNET);
 }
 
+static struct net_icmp_hdr *get_icmp_hdr(struct net_pkt *pkt)
+{
+	net_pkt_cursor_init(pkt);
+
+	net_pkt_skip(pkt, net_pkt_ip_hdr_len(pkt) +
+		     net_pkt_ipv6_ext_len(pkt));
+
+	return (struct net_icmp_hdr *)net_pkt_cursor_get_pos(pkt);
+}
+
 #define NET_ICMP_HDR(pkt) ((struct net_icmp_hdr *)net_pkt_icmp_data(pkt))
 
 static int tester_send(struct device *dev, struct net_pkt *pkt)
 {
-	struct net_icmp_hdr *icmp = NET_ICMP_HDR(pkt);
+	struct net_icmp_hdr *icmp;
 
 	if (!pkt->frags) {
 		TC_ERROR("No data to send!\n");
 		return -ENODATA;
 	}
+
+	icmp = get_icmp_hdr(pkt);
 
 	if (icmp->type == NET_ICMPV6_MLDv2) {
 		/* FIXME, add more checks here */
@@ -353,11 +365,11 @@ static void send_query(struct net_if *iface)
 	net_pkt_append_be16(pkt, 0); /* Resv, S, QRV and QQIC */
 	net_pkt_append_be16(pkt, 0); /* number of addresses */
 
+	net_pkt_set_ipv6_ext_len(pkt, ROUTER_ALERT_LEN);
+
 	net_ipv6_finalize(pkt, NET_IPV6_NEXTHDR_HBHO);
 
 	net_pkt_set_iface(pkt, iface);
-
-	net_pkt_set_ipv6_ext_len(pkt, ROUTER_ALERT_LEN);
 
 	net_pkt_write_be16(pkt, pkt->frags,
 			   NET_IPV6H_LEN + ROUTER_ALERT_LEN + 2,

subsys/net/ip/icmpv6.h
@@ -82,6 +82,14 @@ struct net_icmpv6_mld_query {
 	u16_t num_sources;
 } __packed;
 
+struct net_icmpv6_mld_mcast_record {
+	u8_t record_type;
+	u8_t aux_data_len;
+	u16_t num_sources;
+	struct in6_addr mcast_address;
+} __packed;
+
+
 #define NET_ICMPV6_ND_O_FLAG(flag) ((flag) & 0x40)
 #define NET_ICMPV6_ND_M_FLAG(flag) ((flag) & 0x80)
 

commit bf0ba1418d63868e9ab9ed0256feafcdfee934c6

net: ipv6: mld: fix double net_pkt_unref on send failure
When net_send_data() failed synchronously in mld_send() (e.g. when
the interface is admin-up but not yet carrier-ready), the packet
was unref'd inside mld_send() and then unref'd again by the caller
(net_ipv6_mld_send_single() or send_mld_report()) via its `drop:`
label, producing:

  <err> net_pkt: *** ERROR *** pkt 0x... is freed already
                 by mld_send():138 (net_ipv6_mld_send_single():209)

This was observed on a USB CDC-NCM interface where MLD reports for
auto-joined multicast groups are emitted during early interface
init, before the USB carrier is up. net_send_data() returns a
negative error synchronously, triggering the double-unref.

Remove the inner net_pkt_unref() from mld_send(); both callers
already unref via their existing `drop:` labels.

Add a regression test in tests/net/mld exercising the synchronous
failure path by bringing the iface down before calling
net_ipv6_mld_send_single(). The test enables
CONFIG_NET_PKT_LOG_LEVEL_DBG=y so the existing "freed already"
detection in net_pkt_unref() surfaces the bug in test output on
an unpatched tree.

Bug was introduced in commit 88f5672e4c7 ("net/ipv6: Switch MLD
join, leave and report to new net_pkt API", v1.14.0).

Signed-off-by: Jay Beavers <jay@tolttechnologies.com>
(cherry picked from commit 177611e26335201a8f0edd14b7d39c2cd5092fd8)

subsys/net/ip/ipv6_mld.c
@@ -135,8 +135,7 @@ static int mld_send(struct net_pkt *pkt)
 		net_stats_update_icmp_drop(net_pkt_iface(pkt));
 		net_stats_update_ipv6_mld_drop(net_pkt_iface(pkt));
 
-		net_pkt_unref(pkt);
-
+		/* Caller unrefs pkt via its drop label. */
 		return ret;
 	}
 

tests/net/mld/prj.conf
@@ -25,3 +25,6 @@ CONFIG_NET_IF_UNICAST_IPV6_ADDR_COUNT=4
 CONFIG_NET_IF_MCAST_IPV6_ADDR_COUNT=8
 CONFIG_NET_SOCKETS=y
 CONFIG_NET_IF_MAX_IPV6_COUNT=2
+CONFIG_ASSERT=y
+CONFIG_NET_BUF_LOG=y
+CONFIG_NET_PKT_LOG_LEVEL_DBG=y

tests/net/mld/src/main.c
@@ -1111,4 +1111,40 @@ ZTEST(net_mld_test_suite, test_mld_multi_join)
 	zassert_true(is_leave_msg_ok, "Leave msg invalid");
 }
 
+/* Regression test for a double-free in mld_send() (subsys/net/ip/ipv6_mld.c).
+ *
+ * When net_send_data() failed synchronously (e.g. iface not ready),
+ * mld_send() unref'd the pkt, and net_ipv6_mld_send_single() then
+ * unref'd it again via its `drop:` label. With
+ * CONFIG_NET_PKT_LOG_LEVEL_DBG=y the second unref emits:
+ *   <err> net_pkt: *** ERROR *** pkt 0x... is freed already
+ *                  by mld_send():138 (net_ipv6_mld_send_single():209)
+ *
+ * This test drives the failing path by bringing the iface down before
+ * calling net_ipv6_mld_send_single(). On a buggy tree the error line
+ * above appears in handler.log; on a fixed tree it does not.
+ */
+ZTEST(net_mld_test_suite, test_send_single_no_double_free_on_send_fail)
+{
+	struct net_in6_addr test_mcast = { { { 0xff, 0x02, 0, 0, 0, 0, 0, 0,
+					       0, 0, 0, 0, 0, 0, 0, 0x42 } } };
+	int ret;
+
+	/* Bring the iface down so net_send_data() returns an error
+	 * synchronously — the same path hit during early init on a
+	 * CDC-NCM interface whose USB carrier hasn't come up yet.
+	 */
+	zassert_ok(net_if_down(net_iface), "Failed to bring iface down");
+
+	ret = net_ipv6_mld_send_single(net_iface, &test_mcast,
+				       NET_IPV6_MLDv2_MODE_IS_EXCLUDE);
+
+	zassert_true(ret < 0,
+		     "expected error return from send_single on down iface, got %d",
+		     ret);
+
+	zassert_ok(net_if_up(net_iface), "Failed to bring iface up");
+	k_msleep(THREAD_SLEEP);
+}
+
 ZTEST_SUITE(net_mld_test_suite, NULL, test_mld_setup, test_mld_before, NULL, NULL);